9ee8190d9003e426b3285a5374ca3df71aa4af06
what-the-hub <iamthenight.yeah@gmail.com>
2021-12-20 14:24:44 +0200

    add 4 arrows

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 let gameFlow = document.querySelector('.game')
 let area = document.getElementById("touch-area")
+let scoreElement = document.getElementById('score')
 let scoreCounter = 0 // for score
 let myInterval
 let count = 0 // for checking last element and stop interval
+const blockClasses = ['left-arrow','up-arrow', 'down-arrow', 'right-arrow']
+
+/*class AB {
+    constructor() {
+
+    }
+
+    createItem () {
+        const newItem = document.createElement('div')
+        const newClass = getRandom().itemClass
+        newItem.setAttribute('class',`${newClass} drop-block`)
+        const id = Date.now()
+        document.addEventListener('keydown', {handleEvent: logKey, currentItem: newItem})
+
+        newItem.setAttribute('id', `${id}`)
+        gameFlow.appendChild(newItem)
+        newItem.addEventListener('animationend', () => {
+            //newItem.removeEventListener('keydown', logKey)
+            deleteBlock(newItem)
+        })
+
+        count++
+        if (newCount === count) {
+            clearInterval(myInterval)
+        }
+
+
+
+    }
+
+}*/
 
 
 let areaHeight = parseInt(window.getComputedStyle(area).getPropertyValue('height'))
@@ -12,51 +44,53 @@ console.log(gameFlowHeight, 'game height')
 
 const checkTouch = (item) => {
     let itemPosition = parseInt(window.getComputedStyle(item).getPropertyValue('top'))
-    console.log(itemPosition, item.id)
     let excellentArea = itemPosition >= (gameFlowHeight - 100) + 10 && itemPosition <= gameFlowHeight - 60
     let goodArea = itemPosition >= (gameFlowHeight - 100) - 10 && itemPosition <= gameFlowHeight - 40
+
     if (excellentArea) {
-        console.log('excellent')
         scoreCounter += 2
+        console.log('excellent', scoreCounter)
+
+        scoreElement.textContent = scoreCounter
         //document.getElementById(item.id).remove()
         return
     } else if (goodArea) {
         console.log('correct')
         scoreCounter += 1
+        scoreElement.textContent = scoreCounter
         return
     }
-    console.log(scoreCounter)
 
 }
 
 const deleteBlock = (block) => {
     //document.removeEventListener('keydown', logKey)
     document.getElementById(block.id).remove()
+    console.log(scoreCounter)
 
 
 }
 
 
-const checkClick = () => {
-    let leftBlocks = document.querySelectorAll('.left-arrow')
-    checkTouch(leftBlocks[0])
-}
-const startGame = () => {
-    getRandom()
-
-
+const checkClick = (arrowDirection) => {
+    let columnOfArrows = document.querySelectorAll(arrowDirection)
+    console.log(columnOfArrows)
+    checkTouch(columnOfArrows[0])
 }
 
-function createEl(newCount) {
-    console.log(newCount, 'newCount')
-    document.addEventListener('keydown', logKey)
-    let newItem = document.createElement('div')
-    newItem.classList.add('left-arrow')
-    let id = Date.now()
+
+function createEl(newCount, arrowDirection) {
+    //console.log(newCount, 'newCount')
+    const newItem = document.createElement('div')
+    const newClass = getRandom().itemClass
+    newItem.setAttribute('class',`${newClass} drop-block`)
+    const id = Date.now()
+    document.addEventListener('keydown', {handleEvent: logKey, currentItem: newItem})
+
     newItem.setAttribute('id', `${id}`)
-    console.log('the game is on')
     gameFlow.appendChild(newItem)
     newItem.addEventListener('animationend', () => {
+        //newItem.removeEventListener('keydown', logKey)
         deleteBlock(newItem)
     })
 
@@ -65,29 +99,41 @@ function createEl(newCount) {
         clearInterval(myInterval)
     }
 
-
-
 }
 
 const getRandom = () => {
-    let elements = Math.floor(Math.random() * 8)
-    //let ms = Math.floor(Math.random() * (5000 - 1000) + 1000)
-    console.log(elements, 'elements')
+    const elements = Math.floor((Math.random() * 20) + 5)
+    const seconds = Math.floor(Math.random() * 2000 + 1000)
+    const itemClass = blockClasses[Math.floor(Math.random() * blockClasses.length)]
 
-
-    myInterval = setInterval(createEl, 1000, elements)
-    const allLeft = document.querySelectorAll('.left-arrow')
+    return {
+        elements,
+        seconds,
+        itemClass
+    }
 
 
 }
 
+const startGame = () => {
+    console.log('the game is on')
+    myInterval = setInterval(createEl, 1000, getRandom().elements)
 
-const logKey = (e) => {
+}
+
+
+function logKey (e) {
+    //console.log(this.className, 'className')
+    console.log(this.currentItem)
+
     if (e.key === 'ArrowLeft') {
         //console.log('left arr')
-        checkClick()
+        checkClick('.left-arrow')
     }
-
+    if (e.key === 'ArrowUp') {
+        //console.log('up arr')
+        checkClick('.up-arrow')
+    }
 
 }
 
